40de0db1d744402eb565dbf19d40285938be972d
cenibee <ksj3452@naver.com>
2021-02-05 15:30:14 +0900

    :construction: Navigating by Relationship & Grabbing JSON Schema Metadata

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -12,12 +12,37 @@ class App extends React.Component {
         this.state = {employees: []};
     }
 
-    // DOM 에 React 가 렌더링된 후 실행할 함수
-    componentDidMount() {
+    // 파트 1에서 만들었던 전체 정보를 가져오는 요청
+    loadFromServerAtOnce() {
         axios.get('/api/employees')
             .then(response => {
                 this.setState({employees: response.data._embedded.employees});
             });
+    }
+
+    // Employees 관련 정보를 this.state 에 셋하는 요청
+    loadFromServer(pageSize) {
+        axios.get('/api/employees')
+            .then(employeeCollection => {
+                this.setState({
+                    employees: employeeCollection.data._embedded.employees,
+                    pageSize: pageSize,
+                    links: employeeCollection.data._links
+                });
+
+                axios.get(employeeCollection.data._links.profile.href, {
+                    headers: {'Accept': 'application/schema+json'}
+                }).then(schema => {
+                    this.setState({
+                        attributes: Object.keys(schema.data.properties)
+                    });
+                });
+            });
+    }
+
+    // DOM 에 React 가 렌더링된 후 실행할 함수
+    componentDidMount() {
+        this.loadFromServer(this.state.pageSize);
     }
 
     // 화면에 컴포넌트를 그리도록하는 API - 프레임워크 레벨에서 콜된다.
